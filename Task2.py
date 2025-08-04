3370075c5abbf792329db5f6786b010ea54db4d5
PawanChetri <pawanpokhrelchetri123@gmail.com>
2025-08-04 17:36:33 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,4 @@
+Sum = 0  #To find the sum, first we will create a variable with value 0.
+for i in range(1,51):  #This will run a loop from 1 to 50,i.e, (n-1)
+    Sum += i  #This will add the numbers each time the value of i increases, it gets added up
+print("The same of numbers from 1 to 50 is: {}".format(Sum))  # Prints the last value of Sum variable which is the total.
